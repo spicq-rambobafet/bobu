1dcce73f3b28b4ef0c46fea43822e23558f83f32
Sebastien Picq <picq.seb@gmail.com>
2025-02-03 22:52:07 +0100

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,39 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// 网站配置
+export const SITE_TITLE = "Le blog de Bobu";
+export const SITE_DESCRIPTION = "Bienvenue (. ❛ ᴗ ❛.)";
+export const COPYRIGHT = "© 2025 Bobu Tous droits réservé";
+
+// 社交媒体链接, 留空或注释=不显示
+export const SOCIAL_LINKS = {
+  Github: "https://github.com/spicq-rambobafet",
+  Bluesky: "https://bsky.app/profile/man.tf"
+};
+
+// SEO 相关
+export const SEO_CONFIG = {
+  ogImage: "/hero-img.png", // 默认的社交媒体分享图片
+  keywords: "blog, tech, cuisine, sumo",
+};
+
+// 导航配置
+export const NAV_ITEMS = [
+  { text: "Accueil", link: "/" },
+  { text: "Blog", link: "/blog" },
+  { text: "Tags", link: "/tags" },
+  { text: "A propos", link: "/a-propos" },
+];
+
+// 博客配置
+export const BLOG_CONFIG = {
+  locale: "fr-fr",
+  profile: "https://github.com/spicq-rambobafet",
+  authorName: "Bobu",
+  email: "mailto:your-email@example.com",
+  tags: {
+    title: "Tags",
+    description: "Tous les tags utilisés sur le site",
+  },
+};
