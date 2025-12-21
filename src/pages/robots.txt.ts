1573cdc0b39a8d067c6d07e85f6176e8052b2794
Sebastien Picq <picq.seb@gmail.com>
2025-12-21 16:30:49 +0100

    chore(IA): add IA guidelines in robots

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from "astro";
 
 const getRobotsTxt = (sitemapURL: URL) => `User-agent: *
+Content-Signal: ai-train=no, search=yes, ai-input=yes
 Allow: /
 
 Sitemap: ${sitemapURL.href}
